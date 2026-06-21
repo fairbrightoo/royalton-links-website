dcab77c35f11141f667f44e904f258a4e81b8ac8
Bright Osisiogu <fairbrightoo@gmail.com>
2026-06-21 21:51:34 +0100

    Fix image bundling bug in Vite

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -7,53 +7,62 @@ import TeamCard from './TeamCard';
 
 gsap.registerPlugin(ScrollTrigger);
 
+import team1 from '../assets/images/placeholder_team_1.png';
+import team2 from '../assets/images/placeholder_team_2.png';
+import team3 from '../assets/images/placeholder_team_3.png';
+import team4 from '../assets/images/placeholder_team_4.png';
+import team5 from '../assets/images/placeholder_team_5.png';
+import team6 from '../assets/images/placeholder_team_6.png';
+import team7 from '../assets/images/placeholder_team_7.png';
+import team8 from '../assets/images/placeholder_team_8.png';
+
 // --- 8 MEMBER LAYOUT CONFIGURATION (Unchanged) ---
 const teamMembers = [
     {
         id: 1, name: "Chinedu Okafor", role: "HOD, Accounting Dept",
-        image: "src/assets/images/placeholder_team_1.png",
+        image: team1,
         socials: { linkedin: "#", twitter: "#" },
         style: { top: '0%', left: '2%', width: '16%', aspectRatio: '3/4', zIndex: 2 }
     },
     {
         id: 2, name: "Adebayo Ogunlesi", role: "HOD, Admin Dept",
-        image: "src/assets/images/placeholder_team_2.png",
+        image: team2,
         socials: { linkedin: "#", mail: "#" },
         style: { top: '30%', left: '0%', width: '22%', aspectRatio: '4/3', zIndex: 3 }
     },
     {
         id: 3, name: "Aliyu Abubakar", role: "HOD, Human Resource Dept",
-        image: "src/assets/images/placeholder_team_3.png",
+        image: team3,
         socials: { linkedin: "#" },
         style: { top: '55%', left: '8%', width: '24%', aspectRatio: '1/1', zIndex: 1 }
     },
     {
         id: 4, name: "Dr. Oluwaseun Adeleke", role: "Managing Director",
-        image: "src/assets/images/placeholder_team_4.png",
+        image: team4,
         socials: { linkedin: "#", twitter: "#" },
         style: { top: '2%', left: '26%', width: '22%', aspectRatio: '3/5', zIndex: 2 }
     },
     {
         id: 5, name: "Zainab Usman", role: "General Manager",
-        image: "src/assets/images/placeholder_team_5.png",
+        image: team5,
         socials: { instagram: "#", linkedin: "#" },
         style: { top: '13%', left: '57%', width: '24%', aspectRatio: '3/5', zIndex: 10 }
     },
     {
         id: 6, name: "Nnamdi Chukwu", role: "HOD, Site Dept",
-        image: "src/assets/images/placeholder_team_6.png",
+        image: team6,
         socials: { linkedin: "#", mail: "#" },
         style: { top: '45%', left: '42%', width: '18%', aspectRatio: '3/4', zIndex: 5 }
     },
     {
         id: 7, name: "Amina Bello", role: "Asst. HOD, Business Development",
-        image: "src/assets/images/placeholder_team_7.png",
+        image: team7,
         socials: { linkedin: "#", instagram: "#" },
         style: { top: '5%', right: '2%', width: '18%', aspectRatio: '1/1', zIndex: 3 }
     },
     {
         id: 8, name: "Mr. Tunde Bakare", role: "Asst. General Manager",
-        image: "src/assets/images/placeholder_team_8.png",
+        image: team8,
         socials: { linkedin: "#", twitter: "#" },
         style: { top: '50%', right: '0%', width: '20%', aspectRatio: '4/3', zIndex: 12 }
     },
